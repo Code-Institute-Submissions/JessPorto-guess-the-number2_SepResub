48aa2d8a44e4cd319760b19628cbef6b07b331c6
JessPorto <jessica.portomig@gmail.com>
2022-09-20 09:03:43 +0000

    Add a while True loop to menu function to o collect and validate the user input

--- run.py
+++ run.py
@@ -1,43 +1,54 @@
-# random module imported to generate a random number
+"""
+module imported to generate a random number
+"""
 import random
+
+print("Welcome to Guess the Number II\n")
 
 
 def guessing_game():
-    random_number = random.randint(1, 10)  
+    """
+    Collect and validate the input made by the user
+    and give a chance to guess a higher or lower number
+    """
+    random_number = random.randint(1, 10) 
     while True:
-        text = input('Guess a number from 1 to 10: ')        
+        text = input("Guess a number between 1 to 10: ")
         try:
             player_guess = int(text)
             if player_guess < 1 or player_guess > 10:
-                raise ValueError             
+                raise ValueError
         except ValueError:
-            print('{} is not valid.'.format(text))
+            print("{} is not valid.".format(text))
             continue
         if player_guess != random_number and player_guess < random_number:
             print("Sorry, guess again. Too low")
         elif player_guess != random_number and player_guess > random_number:
             print("Sorry, guess again. Too righ")
         else:
-            break       
+            break
     print(f"Congrats. You have guessed the number {random_number} correctly!")
 
-    
+
 guessing_game()
 
 
 def menu():
+    """
+    Collect and validate entry for a next game
+    """
     while True:
-        print('(1) Play guess number game')
-        print('(Q) Quit')
-        choice = input('Enter your choice: ').lower()
-        if choice == '1':
+        print("(1) Play Guess the Number game")
+        print("(Q) Quit")
+        choice = input("Enter your choice: ").lower()
+        if choice == "1":
             guessing_game()
-        elif choice == 'q':
+        elif choice == "q":
+            print("Thank you!")
             return False
         else:
-            print(f'Not a correct choice: {choice}')
+            print(f"Not a correct choice: {choice}")
 
-            
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     menu()
-
